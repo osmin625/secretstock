8285f87af25a12b8dcefb688de420fbf2df2869f
osmin625 <tmdals179@gmail.com>
2021-11-19 14:00:50 +0900

    stockadd btn and dialog and toast message

--- app/src/main/java/com/example/teamproject/Menu.kt
+++ app/src/main/java/com/example/teamproject/Menu.kt
@@ -34,7 +34,7 @@ class Menu : TabActivity() {
         setContentView(R.layout.activity_menu)
 
         var stockList = ArrayList<Stock>()
-        var addstockBtn = findViewById<Button>(R.id.addstockBtn)
+        var stockAddBtn = findViewById<Button>(R.id.stockAddBtn)
         stockListView = findViewById<ListView>(R.id.stockList)
         var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, stockList)
         stockListView.adapter = adapter
@@ -48,92 +48,92 @@ class Menu : TabActivity() {
         entries.add(BarEntry(6.2f, 30.0f))
         entries.add(BarEntry(7.2f, 90.0f))
 
-            var tabHost = this.tabHost
+        var tabHost = this.tabHost
 
-            var tabSpecChart = tabHost.newTabSpec("Chart").setIndicator("차트")
-            tabSpecChart.setContent(R.id.Chart)
-            tabHost.addTab(tabSpecChart)
+        var tabSpecChart = tabHost.newTabSpec("Chart").setIndicator("차트")
+        tabSpecChart.setContent(R.id.Chart)
+        tabHost.addTab(tabSpecChart)
 
-            var tabSpecStock = tabHost.newTabSpec("Stock").setIndicator("주식")
-            tabSpecStock.setContent(R.id.Stock)
-            tabHost.addTab(tabSpecStock)
+        var tabSpecStock = tabHost.newTabSpec("Stock").setIndicator("주식")
+        tabSpecStock.setContent(R.id.Stock)
+        tabHost.addTab(tabSpecStock)
 
-            var tabSpecWallpaper = tabHost.newTabSpec("Wallpaper").setIndicator("배경")
-            tabSpecWallpaper.setContent(R.id.Wallpaper)
-            tabHost.addTab(tabSpecWallpaper)
+        var tabSpecWallpaper = tabHost.newTabSpec("Wallpaper").setIndicator("배경")
+        tabSpecWallpaper.setContent(R.id.Wallpaper)
+        tabHost.addTab(tabSpecWallpaper)
 
-            var tabSpecSetting = tabHost.newTabSpec("Setting").setIndicator("설정")
-            tabSpecSetting.setContent(R.id.Setting)
-            tabHost.addTab(tabSpecSetting)
+        var tabSpecSetting = tabHost.newTabSpec("Setting").setIndicator("설정")
+        tabSpecSetting.setContent(R.id.Setting)
+        tabHost.addTab(tabSpecSetting)
 
-            var tabSpecTemp = tabHost.newTabSpec("temp").setIndicator("temp")
-            tabSpecTemp.setContent(R.id.temp)
-            tabHost.addTab(tabSpecTemp)
+        //var tabSpecTemp = tabHost.newTabSpec("temp").setIndicator("temp")
+        //tabSpecTemp.setContent(R.id.temp)
+        //tabHost.addTab(tabSpecTemp)
 
-            tabHost.currentTab = 0
+        tabHost.currentTab = 0
 
-            barChart.run {
-                description.isEnabled = false // 차트 옆에 별도로 표기되는 description을 안보이게 설정 (false)
-                setMaxVisibleValueCount(7) // 최대 보이는 그래프 개수를 7개로 지정
-                setPinchZoom(false) // 핀치줌(두손가락으로 줌인 줌 아웃하는것) 설정
-                setDrawBarShadow(false) //그래프의 그림자
-                setDrawGridBackground(false)//격자구조 넣을건지
-                axisLeft.run { //왼쪽 축. 즉 Y방향 축을 뜻한다.
-                    axisMaximum = 101f //100 위치에 선을 그리기 위해 101f로 맥시멈값 설정
-                    axisMinimum = 0f // 최소값 0
-                    granularity = 50f // 50 단위마다 선을 그리려고 설정.
-                    setDrawLabels(true) // 값 적는거 허용 (0, 50, 100)
-                    setDrawGridLines(true) //격자 라인 활용
-                    setDrawAxisLine(false) // 축 그리기 설정
-                    axisLineColor = ContextCompat.getColor(
-                        context,
-                        R.color.design_default_color_secondary_variant
-                    ) // 축 색깔 설정
-                    gridColor = ContextCompat.getColor(
-                        context,
-                        R.color.design_default_color_on_secondary
-                    ) // 축 아닌 격자 색깔 설정
-                    textColor = ContextCompat.getColor(
-                        context,
-                        R.color.design_default_color_primary_dark
-                    ) // 라벨 텍스트 컬러 설정
-                    textSize = 13f //라벨 텍스트 크기
-                }
-                xAxis.run {
-                    position = XAxis.XAxisPosition.BOTTOM //X축을 아래에다가 둔다.
-                    granularity = 1f // 1 단위만큼 간격 두기
-                    setDrawAxisLine(true) // 축 그림
-                    setDrawGridLines(false) // 격자
-                    textColor = ContextCompat.getColor(
-                        context,
-                        R.color.design_default_color_primary_dark
-                    ) //라벨 색상
-                    textSize = 12f // 텍스트 크기
-                    valueFormatter = MyXAxisFormatter() // X축 라벨값(밑에 표시되는 글자) 바꿔주기 위해 설정
-                }
-                axisRight.isEnabled = false // 오른쪽 Y축을 안보이게 해줌.
-                setTouchEnabled(false) // 그래프 터치해도 아무 변화없게 막음
-                animateY(1000) // 밑에서부터 올라오는 애니매이션 적용
-                legend.isEnabled = false //차트 범례 설정
+        barChart.run {
+            description.isEnabled = false // 차트 옆에 별도로 표기되는 description을 안보이게 설정 (false)
+            setMaxVisibleValueCount(7) // 최대 보이는 그래프 개수를 7개로 지정
+            setPinchZoom(false) // 핀치줌(두손가락으로 줌인 줌 아웃하는것) 설정
+            setDrawBarShadow(false) //그래프의 그림자
+            setDrawGridBackground(false)//격자구조 넣을건지
+            axisLeft.run { //왼쪽 축. 즉 Y방향 축을 뜻한다.
+                axisMaximum = 101f //100 위치에 선을 그리기 위해 101f로 맥시멈값 설정
+                axisMinimum = 0f // 최소값 0
+                granularity = 50f // 50 단위마다 선을 그리려고 설정.
+                setDrawLabels(true) // 값 적는거 허용 (0, 50, 100)
+                setDrawGridLines(true) //격자 라인 활용
+                setDrawAxisLine(false) // 축 그리기 설정
+                axisLineColor = ContextCompat.getColor(
+                    context,
+                    R.color.design_default_color_secondary_variant
+                ) // 축 색깔 설정
+                gridColor = ContextCompat.getColor(
+                    context,
+                    R.color.design_default_color_on_secondary
+                ) // 축 아닌 격자 색깔 설정
+                textColor = ContextCompat.getColor(
+                    context,
+                    R.color.design_default_color_primary_dark
+                ) // 라벨 텍스트 컬러 설정
+                textSize = 13f //라벨 텍스트 크기
             }
-
-            var set = BarDataSet(entries, "DataSet") // 데이터셋 초기화
-            set.color = ContextCompat.getColor(
-                applicationContext!!,
-                R.color.design_default_color_primary_dark
-            ) // 바 그래프 색 설정
-
-            val dataSet: ArrayList<IBarDataSet> = ArrayList()
-            dataSet.add(set)
-            val data = BarData(dataSet)
-            data.barWidth = 0.3f //막대 너비 설정
-            barChart.run {
-                this.data = data //차트의 데이터를 data로 설정해줌.
-                setFitBars(true)
-                invalidate()
+            xAxis.run {
+                position = XAxis.XAxisPosition.BOTTOM //X축을 아래에다가 둔다.
+                granularity = 1f // 1 단위만큼 간격 두기
+                setDrawAxisLine(true) // 축 그림
+                setDrawGridLines(false) // 격자
+                textColor = ContextCompat.getColor(
+                    context,
+                    R.color.design_default_color_primary_dark
+                ) //라벨 색상
+                textSize = 12f // 텍스트 크기
+                valueFormatter = MyXAxisFormatter() // X축 라벨값(밑에 표시되는 글자) 바꿔주기 위해 설정
             }
+            axisRight.isEnabled = false // 오른쪽 Y축을 안보이게 해줌.
+            setTouchEnabled(false) // 그래프 터치해도 아무 변화없게 막음
+            animateY(1000) // 밑에서부터 올라오는 애니매이션 적용
+            legend.isEnabled = false //차트 범례 설정
+        }
 
-        addstockBtn.setOnClickListener {
+        var set = BarDataSet(entries, "DataSet") // 데이터셋 초기화
+        set.color = ContextCompat.getColor(
+            applicationContext!!,
+            R.color.design_default_color_primary_dark
+        ) // 바 그래프 색 설정
+
+        val dataSet: ArrayList<IBarDataSet> = ArrayList()
+        dataSet.add(set)
+        val data = BarData(dataSet)
+        data.barWidth = 0.3f //막대 너비 설정
+        barChart.run {
+            this.data = data //차트의 데이터를 data로 설정해줌.
+            setFitBars(true)
+            invalidate()
+        }
+
+        stockAddBtn.setOnClickListener {
             dialogView = View.inflate(this@Menu, R.layout.addstock, null)
             var dlg = AlertDialog.Builder(this@Menu)
             dlg.setTitle("주식 정보 입력")
@@ -161,7 +161,7 @@ class Menu : TabActivity() {
         }
 
 
-        }
+    }
 
 
     inner class MyXAxisFormatter : ValueFormatter() {
