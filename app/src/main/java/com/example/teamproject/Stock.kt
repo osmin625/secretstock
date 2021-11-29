db86bec8ff2ec8b1b2d167d9a963a711d8e77b2b
YuSeongSeong <sndi123456789@gmail.com>
2021-11-30 00:20:50 +0900

    11/30 Search

--- app/src/main/java/com/example/teamproject/Stock.kt
+++ app/src/main/java/com/example/teamproject/Stock.kt
@@ -1,22 +1,20 @@
 package com.example.teamproject
 
 class Stock {
-    var stockCode: Int
+    var stockCode: String
     var stockName: String
     var stockPrice: Int
     var stockNum: Int
 
     constructor() {
-        stockCode = 0
+        stockCode = ""
         stockName = ""
         stockPrice = 0
         stockNum = 0
     }
 
-    constructor(code: Int, name: String, price: Int, num: Int) {
+    constructor(code: String, name: String, price: Int, num: Int) {
         stockCode = code
-
-
         stockName = name
         stockPrice = price
         stockNum = num
