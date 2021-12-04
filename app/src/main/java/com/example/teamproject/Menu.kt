d5a60931d04f33108e7b83130b4ac581f7463568
YuSeongSeong <sndi123456789@gmail.com>
2021-12-04 18:32:59 +0900

    12/4
    dlg fix

--- app/src/main/java/com/example/teamproject/Menu.kt
+++ app/src/main/java/com/example/teamproject/Menu.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.icu.util.ULocale.getName
 import android.os.Bundle
 import android.preference.PreferenceActivity
+import android.text.Layout
 import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -30,6 +31,9 @@ import kotlinx.android.synthetic.main.items.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.addstock.*
+import kotlinx.android.synthetic.main.addstock.view.*
 import kotlin.collections.HashMap as HashMap
 
 
@@ -38,14 +42,9 @@ class Menu : TabActivity() {
     lateinit var user : User
     lateinit var stockAddBtn: Button
     lateinit var logoutbtn: ImageButton
-    lateinit var dlgStockName: EditText
-    lateinit var dlgStockPrice: EditText
-    lateinit var dlgStockCount: EditText
     lateinit var toastText: TextView
-    lateinit var dialogView: View
     lateinit var stockListView: ListView
     lateinit var tempStock: Stock
-    lateinit var stockSearchBtn: ImageButton
     lateinit var resultStockCodeDB:TextView
     lateinit var resultStockNameDB:TextView
     lateinit var stockList : ArrayList<Stock>
@@ -201,16 +200,54 @@ class Menu : TabActivity() {
         stockAddBtn.setOnClickListener {
             var flag = 0
             var StockCode = ""
-            dialogView = View.inflate(this@Menu, R.layout.addstock, null)
-            stockSearchBtn = dialogView.findViewById<ImageButton>(R.id.stockSearchBtn)
-            var dlg = AlertDialog.Builder(this@Menu)
-            dlg.setView(dialogView)
-            dlgStockName = dialogView.findViewById<EditText>(R.id.StockName)
-            dlgStockPrice = dialogView.findViewById<EditText>(R.id.StockPrice)
-            dlgStockCount = dialogView.findViewById<EditText>(R.id.StockCount)
-            dlg.setPositiveButton("확인") { dialog, which ->
-                //var toast1 = Toast(this@Menu)
-                //toastText.text = "주식 추가 완료"
+            var dlgStockName: EditText
+            var dlgStockPrice: EditText
+            var dlgStockCount: EditText
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.addstock, null)
+            val mBuilder = androidx.appcompat.app.AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("")
+            val  mAlertDialog = mBuilder.show()
+
+            dlgStockName = mAlertDialog.StockName
+            dlgStockPrice = mAlertDialog.StockPrice
+            dlgStockCount = mAlertDialog.StockCount
+
+            mDialogView.stockSearchBtn.setOnClickListener {
+                val database = FirebaseDatabase.getInstance()
+                val myRef = database.reference
+                var tempStockName = dlgStockName.text.toString()
+                if(tempStockName == ""){
+                    Toast.makeText(this, "주식정보를 입력하세요.",Toast.LENGTH_SHORT).show()
+                }
+                resultStockCodeDB = mAlertDialog.StockCodeDB
+                resultStockNameDB = mAlertDialog.StockNameDB
+                myRef.child("stock").child(tempStockName).get().addOnSuccessListener {
+                    //Log.i("firebase", "Got value ${it.value}")
+                    if(tempStockName !="") {
+                        resultStockNameDB.text = it.key.toString()
+                        resultStockCodeDB.text = it.value.toString()
+                    }
+                    StockCode = it.value.toString()
+                    flag = 1
+                    for(i in stockList){
+                        Log.i("stockList", "${i.stockCode}")
+                        if(i.stockCode == StockCode) {
+                            Log.i("same stock", "${i.stockCode}")
+                            flag = 2
+                        }
+                    }
+                    if(StockCode.length != 6){
+                        flag = 3
+                    }
+                }.addOnFailureListener{
+                    Log.e("firebase", "Error getting data", it)
+                }
+                Log.i("firebase", "Got value ${StockCode}")
+
+            }
+
+            mDialogView.dlgAddBtn.setOnClickListener {
                 if(flag == 1) {
                     val database = FirebaseDatabase.getInstance()
                     var stockNumber : Int
@@ -253,6 +290,77 @@ class Menu : TabActivity() {
                     Toast.makeText(this, "${tempStock.stockName}이/가 입력되었습니다.",Toast.LENGTH_SHORT).show()
                     newstockList.add(Listviewitem(dlgStockName.text.toString(), stockSum / Integer.parseInt(dlgStockCount.text.toString()), Integer.parseInt(dlgStockCount.text.toString())))
                     adapter.notifyDataSetChanged()
+
+                    j = 1.2f
+                    entries.clear()
+                    for (i in 0..stockChangeNum - 1) {
+                        entries.add(Entry(j, stockChangeList[i].toFloat()))
+                        j = j + 1
+                    }
+
+                    barChart.run {
+                        description.isEnabled = false // 차트 옆에 별도로 표기되는 description을 안보이게 설정 (false)
+                        setMaxVisibleValueCount(5) // 최대 보이는 그래프 개수를 5개로 지정
+                        setPinchZoom(false) // 핀치줌(두손가락으로 줌인 줌 아웃하는것) 설정
+                        //setDrawLineShadow(false) //그래프의 그림자
+                        setDrawGridBackground(false)//격자구조 넣을건지
+
+                        axisLeft.run { //왼쪽 축. 즉 Y방향 축을 뜻한다.
+                            axisMaximum = (stockChangeList[0] * 2).toFloat() //100 위치에 선을 그리기 위해 101f로 맥시멈값 설정
+                            axisMinimum = 0f // 최소값 0
+                            granularity = 50f // 50 단위마다 선을 그리려고 설정.
+                            setDrawLabels(false) // 값 적는거 허용 (0, 50, 100)
+                            //setDrawGridLines(true) //격자 라인 활용
+                            setDrawAxisLine(false) // 축 그리기 설정
+                            setDrawZeroLine(false)
+                            setDrawGridLines(false)
+                            axisLineColor = ContextCompat.getColor(
+                                context,
+                                R.color.design_default_color_secondary_variant
+                            ) // 축 색깔 설정
+                            gridColor = ContextCompat.getColor(
+                                context,
+                                R.color.design_default_color_on_secondary
+                            ) // 축 아닌 격자 색깔 설정
+                            textColor = ContextCompat.getColor(
+                                context,
+                                R.color.design_default_color_primary_dark
+                            ) // 라벨 텍스트 컬러 설정
+                            textSize = 13f //라벨 텍스트 크기
+                        }
+                        xAxis.run {
+                            position = XAxis.XAxisPosition.BOTTOM //X축을 아래에다가 둔다.
+                            granularity = 1f // 1 단위만큼 간격 두기
+                            setDrawAxisLine(true) // 축 그림
+                            setDrawGridLines(false) // 격자
+                            textColor = ContextCompat.getColor(
+                                context,
+                                R.color.design_default_color_primary_dark
+                            ) //라벨 색상
+                            textSize = 12f // 텍스트 크기
+                            valueFormatter = MyXAxisFormatter() // X축 라벨값(밑에 표시되는 글자) 바꿔주기 위해 설정
+                        }
+                        axisRight.isEnabled = false // 오른쪽 Y축을 안보이게 해줌.
+                        setTouchEnabled(false) // 그래프 터치해도 아무 변화없게 막음
+                        animateY(1000) // 밑에서부터 올라오는 애니매이션 적용
+                        legend.isEnabled = false //차트 범례 설정
+                    }
+
+                    var set = LineDataSet(entries, "DataSet") // 데이터셋 초기화
+                    set.color = ContextCompat.getColor(
+                        applicationContext!!,
+                        R.color.design_default_color_primary_dark
+                    ) // 바 그래프 색 설정
+
+                    val dataSet: ArrayList<ILineDataSet> = ArrayList()
+                    dataSet.add(set)
+                    val data = LineData(dataSet)
+                    //data. = 0.3f //막대 너비 설정
+                    barChart.run {
+                        this.data = data //차트의 데이터를 data로 설정해줌
+                        invalidate()
+                    }
+
                 }
                 else if(flag == 2){
                     Toast.makeText(this, "이미 존재하는 주식입니다.",Toast.LENGTH_SHORT).show()
@@ -263,50 +371,15 @@ class Menu : TabActivity() {
                 else{
                     Toast.makeText(this, "주식정보를 입력하세요.",Toast.LENGTH_SHORT).show()
                 }
-                //adapter.notifyDataSetChanged()
-                //toast1.setGravity(Gravity.CENTER, 0, -800)
-                //toast1.show()
+                mAlertDialog.dismiss()
             }
-            dlg.setNegativeButton("취소") { dialog, which ->
-                //var toast2 = Toast(this@Menu)
-                //toastText.text = "취소"
-                //toast2.setGravity(Gravity.CENTER, 0, -800)
-                //toast2.show()
+
+            mDialogView.dlgClose.setOnClickListener {
+                mAlertDialog.dismiss()
             }
-            dlg.show()
-            stockSearchBtn.setOnClickListener {
-                val database = FirebaseDatabase.getInstance()
-                val myRef = database.reference
-                var tempStockName = dlgStockName.text.toString()
-                if(tempStockName == ""){
-                    Toast.makeText(this, "주식정보를 입력하세요.",Toast.LENGTH_SHORT).show()
-                }
-                resultStockCodeDB = dialogView.findViewById(R.id.StockCodeDB)
-                resultStockNameDB = dialogView.findViewById(R.id.StockNameDB)
-                myRef.child("stock").child(tempStockName).get().addOnSuccessListener {
-                    //Log.i("firebase", "Got value ${it.value}")
-                    if(tempStockName !="") {
-                        resultStockNameDB.text = it.key.toString()
-                        resultStockCodeDB.text = it.value.toString()
-                    }
-                    StockCode = it.value.toString()
-                    flag = 1
-                    for(i in stockList){
-                        Log.i("stockList", "${i.stockCode}")
-                        if(i.stockCode == StockCode) {
-                            Log.i("same stock", "${i.stockCode}")
-                            flag = 2
-                        }
-                    }
-                    if(StockCode.length != 6){
-                        flag = 3
-                    }
-                }.addOnFailureListener{
-                    Log.e("firebase", "Error getting data", it)
-                }
-                Log.i("firebase", "Got value ${StockCode}")
-            }
+
         }
+
     }
     inner class ListViewAdapter(private val items: MutableList<Listviewitem>): BaseAdapter() {
         override fun getCount(): Int = items.size
